3572b5eeb1a69bff35ac58bac946f9f0d4a1a3b4
studyrahman03-ops <studyrahman03@gmail.com>
2026-04-08 08:16:58 +0800

    Add files via upload

---
+++ struktur data rahman.py
@@ -0,0 +1,16 @@
+nama = input("masukkan nama pendek anda : ")
+if nama == "rahman":
+    print("selamat datang" , nama)
+else:
+    print("program seesai")
+
+umur = input("masukkan umur anda : ")
+if umur <= "18":
+    print("anda belum cukup umur")
+else:
+    umur >= "18"
+    print("anda cukup umur")
+
+if umur >= "60":
+    print("banyakin ibadah, bentar lagi mati")
+    print("program selesai")
